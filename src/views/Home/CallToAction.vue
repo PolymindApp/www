a034355e940acdc7ac5d9c5b0525ca3e36e46625
Danny Coulombe <contact@dannycoulombe.com>
2022-05-05 08:24:33 -0400

    call to action link

--- src/views/Home/CallToAction.vue
+++ src/views/Home/CallToAction.vue
@@ -4,7 +4,7 @@
             <v-img :src="logo" height="8rem" contain></v-img>
             <h2 class="display-3 mt-6" v-text="$t('home.callToAction.title')"></h2>
             <p class="title mt-3" v-text="$t('home.callToAction.body')"></p>
-            <v-btn light rounded x-large>
+            <v-btn light rounded x-large :href="clientUrl">
                 <v-icon left>mdi-login-variant</v-icon>
                 <span v-text="$t('home.callToAction.openAccount')"></span>
             </v-btn>
@@ -25,6 +25,7 @@ export default {
 
     data: () => ({
         logo,
+        clientUrl: process.env.VUE_APP_CLIENT_URL,
         githubHref: process.env.VUE_APP_GITHUB_URL,
     }),
 }
